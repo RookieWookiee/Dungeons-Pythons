46942da195b2bcbe6f391f2c45bebe3702932916
RookieWookiee <yavorlulchev@gmail.com>
2018-04-25 12:06:31 +0300

    Make those classes Occupiables or Walkables.

--- src/treasure.py
+++ src/treasure.py
@@ -1,8 +1,15 @@
-from src.mixins.print_sym import PrintSymbolMixin
+from src.mixins.print_sym import PrintSymbolMixin as Printable
+from src.mixins.walkable import WalkableMixin as Walkable
 from src.game_object import GameObject
 
 
-class Treasure(PrintSymbolMixin, GameObject):
+class Treasure(GameObject, Walkable, Printable):
+    sym = 'T'
+
     def __init__(self, *, row, col):
         super().__init__(row, col)
-        self.sym = 'T'
+
+    def __on_enter(self, obj):
+        print('Na ti kompot')
+
+    _on_enter = __on_enter
