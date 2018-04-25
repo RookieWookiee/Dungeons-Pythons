699a944a73b5b6cbc858e581eec044eec7e5eb34
RookieWookiee <yavorlulchev@gmail.com>
2018-04-25 11:25:24 +0300

    Add default enter trigger for occupiable mixin.

--- src/mixins/occupiable.py
+++ src/mixins/occupiable.py
@@ -4,3 +4,9 @@ from src.mixins.walkable import WalkableMixin
 class OccupiableMixin(WalkableMixin):
     def __init__(self):
         self.occupant = None
+
+    def trigger_enter_on_occupant(self, *args, **kwargs):
+        if self.occupant is not None:
+            self.occupant._on_enter(*args, **kwargs)
+
+    _on_enter = trigger_enter_on_occupant
